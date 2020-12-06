db68640d169406ee71fd2a16fade29b806af23e2
Tom Bauer <tbauer1898@gmail.com>
2020-12-06 16:53:23 -0600

    Basic file cleanup/organization

---
+++ src/components/pages/CaseTable/CaseTable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './CaseTable.css';
+
+export default function CaseTable() {
+  return (
+    <div className="table">
+      <h2>Here is the Central Case Table</h2>
+    </div>
+  );
+}
