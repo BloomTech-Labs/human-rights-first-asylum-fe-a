f43bfd12e9b8394adc0f6d38b445227db7ca4c06
Tom Bauer <tbauer1898@gmail.com>
2020-12-06 23:03:28 -0600

    Basic table inserted

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import CaseTable from '../Dashboard/CaseTable';
+import React, { useMemo, useState, useEffect } from 'react';
+import CaseTable from '../CaseTable/CaseTable';
+import axios from 'axios';
 // import { Button } from '../../common';
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
+
+  const [fakeData, setFakeData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        setFakeData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
-      <div className="dashboard-container"></div>
+      <div className="dashboard-container">
+        <CaseTable />
+      </div>
 
       <button className="logout-btn" onClick={() => authService.logout()}>
         Logout
