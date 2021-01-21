f2ed198f37e4d2f40b9ba549d3af9cec1d72bca9
Reuben Palumbo <reubensandwichthe1st@gmail.com>
2021-01-21 14:48:26 -0800

    changed colors and styles

--- src/components/pages/Home/Tabs.js
+++ src/components/pages/Home/Tabs.js
@@ -1,34 +1,57 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import styled from 'styled-components';
+import { Button } from 'antd';
 
+const TabItemLeft = styled.button`
+  padding: 0.6em 1em;
+  background: ${props => (props.showCaseTable ? '#215589' : '#7f9bb3')};
+  border: 1px solid black;
+  border-top-left-radius: 20px;
+  outline: none;
+  width: 50%;
+  color: white;
+
+  &:hover {
+    color: white;
+    transform: scaleY(1.1) translateY(-2px);
+  }
+`;
+const TabItemRight = styled.button`
+  padding: 0.6em 1em;
+  background: ${props => (props.showCaseTable ? '#215589' : '#7f9bb3')};
+  border: 1px solid black;
+  border-top-right-radius: 20px;
+  outline: none;
+  width: 50%;
+  color: white;
+
+  &:hover {
+    color: white;
+    transform: scaleY(1.1) translateY(-2px);
+  }
+`;
 const TabDiv = styled.div`
   display: flex;
-  margin: 0 10% 0 0;
-  border: 1px solid black;
-`;
-
-const TabItem = styled.button`
-  padding: 0.6em 1em;
-  background: ${props => (props.showCaseTable ? 'blue' : '#3f51ff')};
-  color: black;
-  border: 1px solid black;
+  margin: 0 5% 0 0;
+  width: 20%;
+  height: 50px;
 `;
 
 function Tabs({ setShowCaseTable, showCaseTable }) {
   return (
     <TabDiv>
-      <TabItem
+      <TabItemLeft
         showCaseTable={showCaseTable}
         onClick={() => setShowCaseTable(true)}
       >
         View Cases
-      </TabItem>
-      <TabItem
+      </TabItemLeft>
+      <TabItemRight
         showCaseTable={!showCaseTable}
         onClick={() => setShowCaseTable(false)}
       >
         View Judges
-      </TabItem>
+      </TabItemRight>
     </TabDiv>
   );
 }
