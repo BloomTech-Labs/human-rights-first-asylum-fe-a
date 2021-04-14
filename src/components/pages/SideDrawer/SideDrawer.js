75acba38503bcbac0cc6bd6b8dd47b72722d9b99
ndossett <niki.m.dossett@gmail.com>
2021-04-14 13:45:51 -0400

    changed padding in SideDrawer header so the top divider matches with header

--- src/components/pages/SideDrawer/SideDrawer.js
+++ src/components/pages/SideDrawer/SideDrawer.js
@@ -63,7 +63,7 @@ const useStyles = makeStyles(theme => ({
   drawerHeader: {
     display: 'flex',
     alignItems: 'center',
-    padding: theme.spacing(0, 1),
+    padding: theme.spacing(2, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
