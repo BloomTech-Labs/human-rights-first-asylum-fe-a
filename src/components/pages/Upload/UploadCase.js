0378e154b8f96ed9e380aafa82eb2f5bb4eba6ab
Matthew Justice <matthewajustice@gmail.com>
2021-04-21 19:21:37 -0400

    sets up scaffolding for back end changes in uploadCase.js

--- src/components/pages/Upload/UploadCase.js
+++ src/components/pages/Upload/UploadCase.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import UploadCaseForm from './UploadCaseForm';
@@ -33,9 +33,11 @@ const useStyles = makeStyles(theme => ({
 
   pdfUpload: {
     marginTop: '15%',
-    display: 'inline-block',
+    display: 'flex',
     marginRight: '7.5%',
     width: '100%',
+    justifyContent: 'center',
+    flexDirection: 'column',
   },
 
   h1Styles: {
@@ -66,7 +68,8 @@ const initialFormValues = {
   initial_or_appellate: '',
   // hearing_type: '' pending stakeholder approval,
   nation_of_origin: '',
-  case_origin: '',
+  case_origin_city: '',
+  case_origin_state: '',
   applicant_perceived_credibility: '',
   case_outcome: '',
   applicant_gender: '',
@@ -84,38 +87,36 @@ const UploadCase = props => {
   const [formValues, setFormValues] = useState(initialFormValues);
   const classes = useStyles();
 
-  const postNewCase = newCase => {
-    axios
-      .post(`${process.env.REACT_APP_API_URI}/upload/`, newCase)
-      .catch(err => console.log(err));
-    setFormValues(initialFormValues);
-  };
+  // const postNewCase = newCase => {
+  //   axios
+  //     .post(`${process.env.REACT_APP_API_URI}/upload/`, newCase)
+  //     .catch(err => console.log(err));
+  //   setFormValues(initialFormValues);
+  // };
 
-  const { handleSubmit } = useForm();
-
-  const onSubmit = () => {
-    const newCase = {
-      case_url: formValues.case_url.trim(),
-      hearing_date: formValues.hearing_date.trim(),
-      judge: formValues.judge.trim(),
-      initial_or_appellate: formValues.initial_or_appellate.trim(),
-      // hearing_type: formValues.hearing_type.trim() (pending stakeholder approval),
-      nation_of_origin: formValues.nation_of_origin.trim(),
-      case_origin: formValues.case_origin.trim(),
-      applicant_perceived_credibility: formValues.applicant_perceived_credibility.trim(),
-      case_outcome: formValues.case_outcome.trim(),
-      applicant_gender: formValues.applicant_gender.trim(),
-      applicant_indigenous_group: formValues.applicant_indigenous_group.trim(),
-      applicant_language: formValues.applicant_language.trim(),
-      type_of_violence_experienced: formValues.type_of_violence_experienced.trim(),
-      applicant_access_to_interpreter: formValues.applicant_access_to_interpreter.trim(),
-      protected_ground: formValues.protected_ground.trim(),
-      application_type: formValues.application_type.trim(),
-      case_filled_within_one_year: formValues.case_filled_within_one_year.trim(),
-      // case_status: formValues..trim() (pending stakeholder approval),
-    };
-    postNewCase(newCase);
-  };
+  // const onSubmit = () => {
+  //   const newCase = {
+  //     case_url: formValues.case_url.trim(),
+  //     hearing_date: formValues.hearing_date.trim(),
+  //     judge: formValues.judge.trim(),
+  //     initial_or_appellate: formValues.initial_or_appellate.trim(),
+  //     // hearing_type: formValues.hearing_type.trim() (pending stakeholder approval),
+  //     nation_of_origin: formValues.nation_of_origin.trim(),
+  //     case_origin: formValues.case_origin.trim(),
+  //     applicant_perceived_credibility: formValues.applicant_perceived_credibility.trim(),
+  //     case_outcome: formValues.case_outcome.trim(),
+  //     applicant_gender: formValues.applicant_gender.trim(),
+  //     applicant_indigenous_group: formValues.applicant_indigenous_group.trim(),
+  //     applicant_language: formValues.applicant_language.trim(),
+  //     type_of_violence_experienced: formValues.type_of_violence_experienced.trim(),
+  //     applicant_access_to_interpreter: formValues.applicant_access_to_interpreter.trim(),
+  //     protected_ground: formValues.protected_ground.trim(),
+  //     application_type: formValues.application_type.trim(),
+  //     case_filled_within_one_year: formValues.case_filled_within_one_year.trim(),
+  //     // case_status: formValues..trim() (pending stakeholder approval),
+  //   };
+  //   postNewCase(newCase);
+  // };
 
   // endpoint to send file to backend
   const onFileChange = e => {
@@ -129,6 +130,7 @@ const UploadCase = props => {
     axios
       .post(`${process.env.REACT_APP_API_URI}/upload/`, dataForm)
       .then(res => {
+        console.log(res);
         setFormValues(res.data);
         alert('Case uploaded successfully');
       })
@@ -148,10 +150,9 @@ const UploadCase = props => {
           <h1 className={classes.h1Styles}>The Case Uploader</h1>
           <h2 className={classes.h2Styles}>
             Upload PDF and wait for form on the right hand side to populate.
-            Please, fill out any remaining empty or incorrect information on the
-            form.
+            Please, adjust any missing or incorrect information
           </h2>
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form>
             <div className="pdf-upload">
               <label htmlFor="btn-upload">
                 <input
@@ -177,7 +178,7 @@ const UploadCase = props => {
       <UploadCaseForm
         formValues={formValues}
         onInputChange={onInputChange}
-        submit={onSubmit}
+        // submit={onSubmit}
       />
     </div>
   );
