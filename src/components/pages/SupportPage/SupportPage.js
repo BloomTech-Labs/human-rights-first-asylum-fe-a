4976c88b8d959af405805129d827622bbaff956d
ruizaj13 <ruizaj13@gmail.com>
2021-04-22 16:18:18 -0700

    expanded FAQ and displayed it with neat collapseable panels

--- src/components/pages/SupportPage/SupportPage.js
+++ src/components/pages/SupportPage/SupportPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Button, Typography, Form, Input } from 'antd';
+import { Button, Typography, Form, Input, Collapse } from 'antd';
 import './SupportPage.css';
+
+const { Panel } = Collapse;
 
 const SupportPage = () => {
   const { Title, Paragraph } = Typography;
@@ -31,18 +33,51 @@ const SupportPage = () => {
       </div>
       <div className="faq">
         <Title level={2}> FAQ </Title>
-        <Title level={4}>Q: How do I upload a case?</Title>
-        <Paragraph>
-          A: Please navigate to the "Upload Case" page via the menu to the left
-          of your screen to upload your case files. Once your files are
-          succesfully uploaded, our system will comb through them and extract
-          the necessary information to then populate the form to the right of
-          the screen on the case upload page for your final approval before
-          completing your submission.
-        </Paragraph>
+        <Collapse>
+          <Panel header={'Q: How do I upload a case?'}>
+            A: Please navigate to the "Upload Case" page via the menu to the
+            left of your screen to upload your case files. Once your files are
+            succesfully uploaded, our system will comb through them and extract
+            the necessary information to then populate the form to the right of
+            the screen on the case upload page for your final approval before
+            completing your submission.
+          </Panel>
+          <Panel header={'Q: Where does your data come from?'}>
+            A: We aggregate data that you, the user, provide. We scrape the data
+            from case files asylum attorneys upload from all over the country
+            and display it for you in a neat, easily digestable package.
+          </Panel>
+          <Panel header={'Q: How often is the data updated?'}>
+            A: The data is updated with every new case file uploaded which is
+            vetted before being added to the database.
+          </Panel>
+          <Panel
+            header={
+              'Q: I know other attorneys that could benefit from this application, how can they gain access?'
+            }
+          >
+            A: Currently the application invite only, please contact the
+            administrator in order to invite fellow attorneys.
+          </Panel>
+          <Panel header={'Q: How secure is your application?'}>
+            A: Both incoming data and new users are vetted manually by an
+            administrator ensuring that both the data and user base remain
+            reliable.
+          </Panel>
+        </Collapse>
       </div>
     </div>
   );
 };
 
 export default SupportPage;
+
+// <Title level={4}>Q: How do I upload a case?</Title>
+// <Paragraph>
+//   A: Please navigate to the "Upload Case" page via the menu to the left
+//   of your screen to upload your case files. Once your files are
+//   succesfully uploaded, our system will comb through them and extract
+//   the necessary information to then populate the form to the right of
+//   the screen on the case upload page for your final approval before
+//   completing your submission.
+// </Paragraph>
