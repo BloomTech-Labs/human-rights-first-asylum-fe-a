da4cc2b20bf7c0bd3999ea8f9e168e4db88afdc3
ndossett <niki.m.dossett@gmail.com>
2021-04-22 10:56:04 -0400

    added css file, antd components, and moved styling from js

--- src/components/pages/SupportPage/SupportPage.js
+++ src/components/pages/SupportPage/SupportPage.js
@@ -1,88 +1,45 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    marginTop: '7%',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-around',
-  },
-  form: {
-    width: '40%',
-  },
-  faq: {
-    width: '40%',
-  },
-  h4Styles: {
-    fontSize: '1.1rem',
-    marginBottom: '1.5rem',
-  },
-  h2Styles: {
-    fontSize: '1.3rem',
-    marginBottom: '2.5rem',
-    width: '100%',
-  },
-  textField: {
-    width: '100%',
-    margin: '0 auto',
-  },
-  buttonStyles: {
-    color: '#ffffff',
-    backgroundColor: '#215589',
-    marginTop: '3%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-}));
+import { Button, Typography, Form, Input } from 'antd';
+import './SupportPage.css';
 
 const SupportPage = () => {
-  const classes = useStyles();
-
-  const onSubmit = e => {
-    e.preventDefault();
-  };
+  const { Title, Paragraph } = Typography;
 
   return (
-    <div className={classes.root}>
-      <div className={classes.form}>
-        <h2 className={classes.h2Styles}> Contact Us </h2>
-        <form>
-          <label htmlFor="message">
-            <TextField
-              id="message"
-              label="Message"
-              type="text"
-              variant="outlined"
-              className={classes.textField}
-            />
-          </label>
-          <div className="submit-button">
-            <Button
-              onClick={onSubmit}
-              className={classes.buttonStyles}
-              variant="contained"
-              component="span"
-            >
+    <div className="root">
+      <div className="form">
+        <Title level={2}> Contact Us </Title>
+        <Form>
+          <Form.Item
+            label="Message"
+            name="Message"
+            rules={[
+              {
+                required: true,
+                message: 'Please input a message',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
               Submit
             </Button>
-          </div>
-        </form>
+          </Form.Item>
+        </Form>
       </div>
-      <div className={classes.faq}>
-        <h2 className={classes.h2Styles}>FAQ</h2>
-        <h4 className={classes.h4Styles}>Q: How do I upload a case?</h4>
-        <p>
+      <div className="faq">
+        <Title level={2}> FAQ </Title>
+        <Title level={4}>Q: How do I upload a case?</Title>
+        <Paragraph>
           A: Please navigate to the "Upload Case" page via the menu to the left
           of your screen to upload your case files. Once your files are
           succesfully uploaded, our system will comb through them and extract
           the necessary information to then populate the form to the right of
           the screen on the case upload page for your final approval before
           completing your submission.
-        </p>
+        </Paragraph>
       </div>
     </div>
   );
