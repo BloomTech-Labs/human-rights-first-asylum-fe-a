bb606abdbae70cad2d9d19f1576873318370a930
ruizaj13 <ruizaj13@gmail.com>
2021-04-26 14:37:48 -0700

    refactored FAQ code to use map function in anticipation of future add/edit FAQ ui feature requested by stakeholder

--- src/components/pages/SupportPage/SupportPage.js
+++ src/components/pages/SupportPage/SupportPage.js
@@ -2,6 +2,35 @@ import React from 'react';
 import { Button, Typography, Form, Input, Collapse } from 'antd';
 import './SupportPage.css';
 
+const FAQ = [
+  {
+    question: 'How do I upload a case?',
+    answer:
+      'Please navigate to the "Upload Case" page via the menu to the left of your screen to upload your case files. Once your files are succesfully uploaded, our system will comb through them and extract the necessary information to then populate the form to the right of the screen on the case upload page for your final approval before completing your submission.',
+  },
+  {
+    question: 'Where does your data come from?',
+    answer:
+      'We aggregate data that you, the user, provide. We scrape the data from case files asylum attorneys upload from all over the country and display it for you in a neat, easily digestable package.',
+  },
+  {
+    question: 'How often is the data updated?',
+    answer:
+      'The data is updated with every new case file uploaded which is vetted before being added to the database.',
+  },
+  {
+    question:
+      'I know other attorneys that could benefit from this application, how can they gain access?',
+    answer:
+      'Currently, the application is invite only. Please contact the administrator in order to invite fellow attorneys.',
+  },
+  {
+    question: 'How secure is your application?',
+    answer:
+      'Both incoming data and new users are vetted manually by an administrator ensuring that both the data and user base remain reliable.',
+  },
+];
+
 const SupportPage = () => {
   const { Title } = Typography;
   const { Panel } = Collapse;
@@ -34,36 +63,11 @@ const SupportPage = () => {
       <div className="faq">
         <Title level={2}> FAQ </Title>
         <Collapse>
-          <Panel header={'Q: How do I upload a case?'}>
-            A: Please navigate to the "Upload Case" page via the menu to the
-            left of your screen to upload your case files. Once your files are
-            succesfully uploaded, our system will comb through them and extract
-            the necessary information to then populate the form to the right of
-            the screen on the case upload page for your final approval before
-            completing your submission.
-          </Panel>
-          <Panel header={'Q: Where does your data come from?'}>
-            A: We aggregate data that you, the user, provide. We scrape the data
-            from case files asylum attorneys upload from all over the country
-            and display it for you in a neat, easily digestable package.
-          </Panel>
-          <Panel header={'Q: How often is the data updated?'}>
-            A: The data is updated with every new case file uploaded which is
-            vetted before being added to the database.
-          </Panel>
-          <Panel
-            header={
-              'Q: I know other attorneys that could benefit from this application, how can they gain access?'
-            }
-          >
-            A: Currently, the application is invite only. Please contact the
-            administrator in order to invite fellow attorneys.
-          </Panel>
-          <Panel header={'Q: How secure is your application?'}>
-            A: Both incoming data and new users are vetted manually by an
-            administrator ensuring that both the data and user base remain
-            reliable.
-          </Panel>
+          {FAQ.map(item => {
+            return (
+              <Panel header={`Q: ${item.question}`}>A: {item.answer}</Panel>
+            );
+          })}
         </Collapse>
       </div>
     </div>
