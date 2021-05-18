5969008ff59520b90d9da71aafd45c1f988fa345
katieolson84 <katieolson84@gmail.com>
2021-05-17 22:24:01 -0600

    updated styling for account drop-down

--- src/components/pages/Home/MainHeader.js
+++ src/components/pages/Home/MainHeader.js
@@ -5,6 +5,9 @@ import { Menu, Dropdown } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { useHistory } from 'react-router-dom';
 
+import Icon from '@ant-design/icons';
+import Profile from '../../../styles/icons/profile.svg';
+
 export default function MainHeader(props) {
   const { logout } = props;
   const history = useHistory();
@@ -19,20 +22,26 @@ export default function MainHeader(props) {
 
   const menu = (
     <Menu onClick={onClick}>
-      <Menu.Item key="/account">Account</Menu.Item>
-      <Menu.Item key="/support">Support</Menu.Item>
-      <Menu.Item key="/logout">Log Out</Menu.Item>
+      <Menu.Item className="account" key="/account">
+        Account
+      </Menu.Item>
+      <Menu.Item className="support" key="/support">
+        Support
+      </Menu.Item>
+      <Menu.Item className="log-out" key="/logout">
+        Log Out
+      </Menu.Item>
     </Menu>
   );
   return (
     <div className="site-page-header">
       <img src={HRFLogo} alt="HRF Logo" />
-      <Dropdown overlay={menu}>
-        <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+      <Dropdown className="drop-down" overlay={menu}>
+        <ul className="ant-dropdown-link" onClick={e => e.preventDefault()}>
           <div className="user-button">
-            <UserOutlined />
+            <Icon component={() => <img src={Profile} alt="profile icon" />} />
           </div>
-        </a>
+        </ul>
       </Dropdown>
     </div>
   );
