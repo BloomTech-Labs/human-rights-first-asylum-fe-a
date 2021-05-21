b3df668ed954372d52caa379cfbb2f25d349629c
katieolson84 <katieolson84@gmail.com>
2021-05-21 12:31:30 -0600

    Converted 'Add a FAQ' to a modal

--- src/components/pages/AdminTools/AddFaq.js
+++ src/components/pages/AdminTools/AddFaq.js
@@ -1,46 +1,54 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+// import { makeStyles } from '@material-ui/core/styles';
+// import TextField from '@material-ui/core/TextField';
+// import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import { Form, Input, Button, Modal } from 'antd';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    marginTop: '7%',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-around',
-  },
-  form: {
-    width: '50%',
-  },
-  h1Styles: {
-    fontSize: '2rem',
-    marginBottom: '2.5rem',
-  },
-  h2Styles: {
-    fontSize: '1.3rem',
-    marginBottom: '2.5rem',
-    width: '100%',
-  },
-  textField: {
-    width: '100%',
-    margin: '1% auto',
-  },
-  buttonStyles: {
-    color: '#ffffff',
-    backgroundColor: '#215589',
-    marginTop: '3%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  radio: {
-    margin: '8%',
-    fontSize: '1.5rem',
-  },
-}));
+import './_FaqStyles.less';
+import { set } from 'date-fns';
+
+import Icon from '@ant-design/icons';
+import OrangeLine from '../../../styles/orange-line.svg';
+
+const { TextArea } = Input;
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     marginTop: '7%',
+//     width: '100%',
+//     display: 'flex',
+//     justifyContent: 'space-around',
+//   },
+//   form: {
+//     width: '50%',
+//   },
+//   h1Styles: {
+//     fontSize: '2rem',
+//     marginBottom: '2.5rem',
+//   },
+//   h2Styles: {
+//     fontSize: '1.3rem',
+//     marginBottom: '2.5rem',
+//     width: '100%',
+//   },
+//   textField: {
+//     width: '100%',
+//     margin: '1% auto',
+//   },
+//   buttonStyles: {
+//     color: '#ffffff',
+//     backgroundColor: '#215589',
+//     marginTop: '3%',
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   radio: {
+//     margin: '8%',
+//     fontSize: '1.5rem',
+//   },
+// }));
 
 const initialFormValues = {
   question: '',
@@ -52,7 +60,20 @@ const AddFaq = props => {
   const { authState } = props;
   const history = useHistory();
 
-  const classes = useStyles();
+  // My added code for modal
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   const onChange = e => {
     const { name, value } = e.target;
@@ -81,47 +102,55 @@ const AddFaq = props => {
   };
 
   return (
-    <div className={classes.root}>
-      <div className={classes.form}>
-        <h2 className={classes.h1Styles}> Add Question </h2>
-        <form onSubmit={onSubmit}>
-          <label htmlFor="question">
-            <TextField
-              id="question"
-              label="Question"
-              type="text"
-              name="question"
-              variant="outlined"
-              multiline={true}
-              onChange={onChange}
-              className={classes.textField}
-              value={formValues.question}
-            />
-          </label>
-          <label htmlFor="answer">
-            <TextField
-              id="answer"
-              label="Answer"
-              type="text"
-              name="answer"
-              variant="outlined"
-              multiline={true}
-              onChange={onChange}
-              className={classes.textField}
-              value={formValues.answer}
-            />
-          </label>
-          <div className="submit-button">
-            <Button
-              onClick={onSubmit}
-              className={classes.buttonStyles}
-              variant="contained"
-              component="span"
-            >
-              Submit
-            </Button>
-          </div>
-        </form>
+    <div className="root">
+      <div className="add-faq-btn-container">
+        <Button className="add-faq-btn" onClick={showModal}>
+          Add a FAQ
+        </Button>
+        <Modal
+          title=""
+          visible={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          footer={[
+            <div className="submit-button">
+              <Button onClick={onSubmit} className="btn-style">
+                <span>Submit</span>
+              </Button>
+            </div>,
+          ]}
+        >
+          <Form onSubmit={onSubmit} layout="vertical" className="faq-form">
+            <h2 className="h1Styles">Add a FAQ</h2>
+            <p className="divider">
+              <Icon
+                component={() => <img src={OrangeLine} alt="divider icon" />}
+              />
+            </p>
+            <Form.Item label="Question">
+              <TextArea
+                id="question"
+                type="text"
+                name="question"
+                autoSize={{ minRows: 2, maxRows: 8 }}
+                onChange={onChange}
+                className="text-field"
+                value={formValues.question}
+              />
+            </Form.Item>
+            <Form.Item label="Answer">
+              <TextArea
+                id="answer"
+                type="text"
+                name="answer"
+                autoSize={{ minRows: 4, maxRows: 10 }}
+                onChange={onChange}
+                className="text-field"
+                value={formValues.answer}
+              />
+            </Form.Item>
+          </Form>
+        </Modal>
       </div>
     </div>
   );
