ac84aab934f52c5d3674e5a17a0c0d7b69f8baa5
Sebastian <72098066+sebastian-espeset@users.noreply.github.com>
2021-05-21 16:56:18 -0500

    adds an axiosWithAuth function to begin refactoring axios calls (#193)

---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('okta-token-storage');
+  const parsedToken = JSON.parse(token);
+
+  return axios.create({
+    baseURL: process.env.REACT_APP_API_URI,
+    headers: {
+      Authorization: 'Bearer ' + parsedToken.idToken.value,
+    },
+  });
+};
