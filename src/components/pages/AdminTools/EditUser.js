3fdb2fd0ae282f938146e327124530da2e0d877d
Michelle Chen <68928523+MichelleCCodes@users.noreply.github.com>
2021-05-24 03:36:35 -0700

    Removed unneccessary import

--- src/components/pages/AdminTools/EditUser.js
+++ src/components/pages/AdminTools/EditUser.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useImperativeHandle } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
