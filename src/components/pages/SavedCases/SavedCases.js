10a19b59efd9cfb74c705ed9c1455050ffe23077
Michelle Chen <mchen0062@gmail.com>
2021-05-25 19:13:00 -0700

    fixed width for each column

--- src/components/pages/SavedCases/SavedCases.js
+++ src/components/pages/SavedCases/SavedCases.js
@@ -9,39 +9,48 @@ import './_SavedCasesStyles.less';
 function SavedCases({ savedCases, deleteBookmark }) {
   const columns = [
     {
-      title: 'Case Number',
-      dataIndex: 'case_number',
-      key: 'case_number',
-      render: text => <Link to={`/case/${text}`}>{text}</Link>,
+      title: 'View Case',
+      dataIndex: 'case_id',
+      key: 'case_id',
+      render: (text, record) => (
+        <Link to={`/case/${record.case_id}`}>{text}</Link>
+      ),
+      width: '15%',
     },
     {
       title: 'Country of Origin',
       dataIndex: 'country_of_origin',
       key: 'country_of_origin',
+      width: '20%',
     },
     {
       title: 'Protected Grounds',
       dataIndex: 'protected_grounds',
       key: 'protected_grounds',
+      width: '20%',
     },
     {
       title: 'Application Type',
       dataIndex: 'application_type',
       key: 'application_type',
+      width: '20%',
     },
     {
       title: 'Judge',
       dataIndex: 'judge_id',
       key: 'judge_id',
+      width: '15%',
     },
     {
       title: 'Outcome',
       dataIndex: 'case_outcome',
       key: 'case_outcome',
+      width: '15%',
     },
     {
       title: 'Remove',
       key: 'remove',
+      width: '5%',
       render: (text, record) => (
         <Icon
           onClick={() => deleteBookmark(record.case_id)}
@@ -53,7 +62,7 @@ function SavedCases({ savedCases, deleteBookmark }) {
   return (
     <div className="savedCasesContainer">
       <div className="savedCases">
-        <h2 className="saved-cases-header">Review Cases</h2>
+        <h2 className="saved-cases-header">Saved Cases</h2>
         <p className="divider">
           <Icon component={() => <img src={OrangeLine} alt="divider icon" />} />
         </p>
