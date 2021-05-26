e925bb96b072bb628e4232dae9f4a8ef93dc4f35
Chad Cox <chad@Chads-MacBook-Pro.local>
2021-05-25 21:23:48 -0700

    converted judges table to antD added column sort and search fixed search placeholder

--- src/components/pages/JudgeTable/JudgeTable.js
+++ src/components/pages/JudgeTable/JudgeTable.js
@@ -1,72 +1,210 @@
 import React, { useState } from 'react';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
 import { Link } from 'react-router-dom';
-
-import { DataGrid } from '@material-ui/data-grid';
-
-import { Button, Input, Card, Drawer, Typography } from 'antd';
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
+import { PageHeader, Table, Space, Button, Input } from 'antd';
 import './_JudgeTableStyles.less';
 
 import FeatherIcon from 'feather-icons-react';
 
 export default function JudgeTable(props) {
   const { judgeData, userInfo, savedJudges, setSavedJudges, authState } = props;
-  const [selectionModel, setSelectionModel] = useState([]);
+  const [state, setState] = useState({
+    searchText: '',
+    searchedColumn: '',
+    selectedRowID: [],
+    selectionModel: [],
+  });
+
+  const { searchText, searchedColumn, selectedRowID, selectionModel } = state;
+
+  let judgesData = judgeData.map(judges => ({
+    judge_name:
+      judges.last_name +
+      ', ' +
+      judges.first_name +
+      ' ' +
+      judges.middle_initial +
+      '.',
+    ...judges,
+  }));
+
+  const onSelectChange = selectedRowID => {
+    console.log('selectedRowID changed: ', selectedRowID);
+    setState({ selectedRowID });
+  };
+
+  const getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          type="text"
+          id="searchInput"
+          placeholder={`Search ${dataIndex.replace(/_/g, ' ')}`}
+          value={selectedKeys[0]}
+          onChange={e =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setState({
+                searchText: selectedKeys[0],
+                searchedColumn: dataIndex,
+              });
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : '',
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        //setTimeout(() => searchInput.select(), 100);
+      }
+    },
+    render: text =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+  const handleReset = clearFilters => {
+    clearFilters();
+    setState({ searchText: '' });
+  };
+
+  function changeSorter(sorter) {
+    console.log(sorter);
+  }
+
+  const rowSelection = {
+    selectedRowID,
+    onChange: onSelectChange,
+  };
+
+  function formatDate(text) {
+    var year = text.slice(0, 4);
+    var month = text.slice(5, 7);
+    var day = text.slice(8, 10);
+    return month + '/' + day + '/' + year;
+  }
 
   const columns = [
     {
-      field: 'first_name',
-      renderHeader: params => <strong>{'Judge'}</strong>,
-      width: 170,
-      headerName: 'Name',
-      options: {
-        filter: true,
-      },
-      //Link to individual judge page
-
-      renderCell: params => (
-        <>
-          <Link to={`/judge/${params.row.judge_id}`} className="judgeTableLink">
-            <span>{params.value}</span>
-          </Link>
-        </>
-      ),
+      title: 'Judge Name',
+      dataIndex: 'judge_name',
+      key: 'judge_name',
+      sorter: (a, b) => a.last_name.localeCompare(b.last_name),
+      sortDirections: ['descend', 'ascend'],
+      ...getColumnSearchProps('judge_name'),
+      render: text => <Link to={`/judge/${judgesData.judge_id}`}>{text}</Link>,
+    },
+    //Link to individual judge page
+    //   renderCell: params => (
+    //     <>
+    //       <Link to={`/judge/${params.row.judge_id}`} className="judgeTableLink">
+    //         <span>{params.value}</span>
+    //       </Link>
+    //     </>
+    //   ),
+    // },
+    {
+      title: 'Judge County',
+      dataIndex: 'judge_county',
+      key: 'judge_county',
+      sorter: (a, b) => a.judge_county.localeCompare(b.judge_county),
+      sortDirections: ['descend', 'ascend'],
+      ...getColumnSearchProps('judge_county'),
     },
     {
-      field: 'judge_county',
-      renderHeader: params => <strong>{'Case Origin'}</strong>,
-      headerName: 'Case Origin',
-      width: 160,
+      title: 'Date Appointed',
+      dataIndex: 'date_appointed',
+      key: 'date_appointed',
+      sorter: (a, b) => a.date_appointed.localeCompare(b.date_appointed),
+      sortDirections: ['descend', 'ascend'],
+      ...getColumnSearchProps('date_appointed'),
+      render: text => formatDate(text),
     },
     {
-      field: 'date_appointed',
-      renderHeader: params => <strong>{'Date Appointed'}</strong>,
-      headerName: 'Date Appointed',
-      width: 160,
+      title: 'Appointed By',
+      dataIndex: 'appointed_by',
+      key: 'appointed_by',
+      sorter: (a, b) => a.appointed_by.localeCompare(b.appointed_by),
+      sortDirections: ['descend', 'ascend'],
+      ...getColumnSearchProps('appointed_by'),
     },
     {
-      field: 'appointed_by',
-      renderHeader: params => <strong>{'Appointed By'}</strong>,
-      headerName: 'Appointed By',
-      width: 160,
+      title: 'Denial Rate',
+      dataIndex: 'denial_rate',
+      key: 'denial_rate',
+      sorter: (a, b) => a.denial_rate.localeCompare(b.denial_rate),
+      sortDirections: ['descend', 'ascend'],
+      ...getColumnSearchProps('denial_rate'),
     },
     {
-      field: 'denial_rate',
-      renderHeader: params => <strong>{'% Denial'}</strong>,
-      headerName: 'Denial Rate',
-      width: 110,
-    },
-    {
-      field: 'approval_rate',
-      renderHeader: params => <strong>{'% Approval'}</strong>,
-      headerName: 'Approval Rate',
-      width: 130,
+      title: 'Approval Rate',
+      dataIndex: 'approval_rate',
+      key: 'approval_rate',
+      sorter: (a, b) => a.approval_rate.localeCompare(b.approval_rate),
+      sortDirections: ['descend', 'ascend'],
+      ...getColumnSearchProps('approval_rate'),
     },
   ];
-
-  judgeData.forEach((item, idx) => {
-    item.id = idx;
-  }); // this is VERY hacky, but the table doesn't take data without ids
 
   const findRowByID = (rowID, rowData) => {
     for (let i = 0; i < rowData.length; i++) {
@@ -118,159 +256,32 @@ export default function JudgeTable(props) {
     }
   };
 
-  const Toolbar = () => {
-    const { Title } = Typography;
-    return (
-      <div className="menuContainer">
-        <Title level={2}>Judges</Title>
-        <div className="buttonContainer">
-          <Button
-            onClick={() => {
-              toggleSearch();
-            }}
-          >
-            <FeatherIcon icon="search" />
-          </Button>
-          <Button
-            onClick={() => {
-              bookmarkJudges(selectionModel);
-            }}
-          >
-            <FeatherIcon icon="bookmark" />
-          </Button>
-        </div>
-      </div>
-    );
-  };
-
-  const [queryValues, setQueryValues] = useState({
-    name: '',
-    judge_county: '',
-    date_appointed: '',
-    appointed_by: '',
-    denial_rate: '',
-    approval_rate: '',
-  });
-
-  const [new_search, setSearch] = useState(false);
-  const toggleSearch = () => {
-    setSearch(!new_search);
-  };
-  const [searching, setSearching] = useState(false);
-
-  const filter = data => {
-    // searchedKeys is AT MOST 16 keys
-    const searchedKeys = Object.entries(queryValues).filter(
-      ([k, v]) => v !== ''
-    );
-    // for each ROW in DATA -- O(n) where n is the number of rows in our data
-    const filteredData = data.filter(row => {
-      const matchedHits = [];
-      // map through each searched [K, V] pair -- O(searched_keys) where searchedKeys is at min 0 and at most 16
-      // so nesting this inside is NOT all too expensive.
-      searchedKeys.forEach(([k, v]) => {
-        // if the stringified value at row[key] includes the searched-for value,
-        // then we'll push the key to our matchedHits
-        if (row[k].toString().includes(v.toString())) {
-          matchedHits.push(k);
-        }
-      });
-      // if the row[k] == v at EVERY searched-for key, then we'll return TRUE
-      // else return FALSE
-      return matchedHits.length === searchedKeys.length;
-    });
-    // filteredData is only going to contain rows where  every
-    // searched column includes subtext that matches the searched term
-    return filteredData;
-  };
-
-  const searchOptions = [
-    { id: 'name', label: 'Judge' },
-    { id: 'judge_county', label: 'Case Origin' },
-    { id: 'date_appointed', label: 'Date Appointed' },
-    { id: 'appointed_by', label: 'Appointed by' },
-    { id: 'denial_rate', label: '% Denial' },
-    { id: 'approval_rate', label: '% Approval' },
-  ];
-  const drawerContent = () => {
-    return (
-      <div className="judgeTableDrawer">
-        {searchOptions.map(value => {
-          return (
-            <div key={value.id}>
-              <p>{value.label}</p>
-              <Input
-                placeholder={'search query'}
-                variant="outlined"
-                size="medium"
-                value={queryValues[value.id]}
-                onChange={e => {
-                  setQueryValues({
-                    ...queryValues,
-                    [value.id]: e.target.value,
-                  });
-                  setSearching(true);
-                }}
-                type="text"
-                className="judgePageSearchInput"
-              />
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
-
   return (
-    <div className="judgeTableContainer">
-      {searching && (
-        <div className="judgeTableCard">
-          {searchOptions.map(option => {
-            if (queryValues[option.id] !== '') {
-              return (
-                <Card
-                  key={option.id}
-                  label={`${option.label}: "${queryValues[option.id]}"`}
-                  onDelete={() => {
-                    setQueryValues({
-                      ...queryValues,
-                      [option.id]: '',
-                    });
-                  }}
-                />
-              );
-            } else {
-              return null;
-            }
-          })}
+    console.log(judgesData),
+    (
+      <div className="judgeContainer">
+        <div className="judgeTableContainer">
+          <div className="table-header">
+            <h2>Judges</h2>
+            <Button
+              className="buttonContainer"
+              onClick={() => {
+                bookmarkJudges(selectedRowID);
+              }}
+            >
+              <FeatherIcon icon="bookmark" />
+            </Button>
+          </div>
+          <Table
+            className="judges_table"
+            rowSelection={rowSelection}
+            rowKey={record => record.judge_id}
+            columns={columns}
+            dataSource={judgesData}
+            onChange={changeSorter}
+          />
         </div>
-      )}
-      <Drawer
-        visible={new_search}
-        onClose={toggleSearch}
-        width={'25%'}
-        style={{ marginTop: '4rem' }}
-      >
-        {drawerContent()}
-      </Drawer>
-
-      <div className="judgeTableGridContainer">
-        <DataGrid
-          rows={searching ? filter(judgeData) : judgeData}
-          columns={columns}
-          className="judgeTable"
-          autoHeight={true}
-          loading={judgeData ? false : true}
-          checkboxSelection={true}
-          onSelectionModelChange={newSelection => {
-            setSelectionModel(newSelection.selectionModel);
-          }}
-          selectionModel={selectionModel}
-          showCellRightBorder={false}
-          showColumnRightBorder={false}
-          components={{ Toolbar: Toolbar }}
-        />
       </div>
-    </div>
+    )
   );
 }
