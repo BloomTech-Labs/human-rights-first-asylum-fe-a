ed2a6f97e3eda0650a5975215e03be94ab68d14b
Lindell Carter <lindellcarternyc@gmail.com>
2021-06-11 16:56:18 -0400

    implements notifications as ANT Design List Items; adds actions to be handled by provided 'view' and 'dismiss' callbacks

--- src/components/pages/Home/Notifications.js
+++ src/components/pages/Home/Notifications.js
@@ -1,27 +1,94 @@
 import React from 'react';
-import { Badge, Dropdown, Menu } from 'antd';
+import { Badge, Card, Dropdown, List, Menu } from 'antd';
 import { BellOutlined } from '@ant-design/icons';
 
-export const NotificationsList = props => {
-  const { notifications } = props;
+const NotificationListItem = props => {
+  const { notification, onClickView, onClickDismiss } = props;
+
+  const handleClick = key => {
+    return evt => {
+      evt.preventDefault();
+
+      if (key === 'view') {
+        return onClickView(notification.id);
+      } else if (key === 'dismiss') {
+        return onClickDismiss(notification.id);
+      }
+    };
+  };
+
   return (
-    <Menu>
-      {notifications.map((notification, idx) => {
-        return <Menu.Item key={idx}>{notification.text}</Menu.Item>;
-      })}
+    <List.Item
+      style={{
+        padding: '0.5rem',
+        marginBottom: '1rem',
+      }}
+      actions={[
+        <a
+          href="#"
+          style={{ marginRight: '0.5rem' }}
+          onClick={handleClick('view')}
+        >
+          View
+        </a>,
+        <a href="#" onClick={handleClick('dismiss')}>
+          Dismiss
+        </a>,
+      ]}
+    >
+      <List.Item.Meta
+        title={notification.title}
+        description={notification.text}
+      />
+    </List.Item>
+  );
+};
+
+export const NotificationsList = props => {
+  const { notifications, onClickView, onClickDismiss } = props;
+  return (
+    <Menu
+      style={{
+        minWidth: '500px',
+      }}
+    >
+      <List
+        style={{
+          padding: '1rem',
+        }}
+        dataSource={notifications}
+        renderItem={notification => {
+          return (
+            <NotificationListItem
+              notification={notification}
+              onClickView={onClickView}
+              onClickDismiss={onClickDismiss}
+            />
+          );
+        }}
+      />
     </Menu>
   );
 };
 
 export default function Notifications(props) {
-  const { notifications } = props;
+  const { notifications, onClickDismiss, onClickView } = props;
 
   return (
-    <Dropdown overlay={<NotificationsList notifications={notifications} />}>
+    <Dropdown
+      overlay={
+        <NotificationsList
+          notifications={notifications}
+          onClickDismiss={onClickDismiss}
+          onClickView={onClickView}
+        />
+      }
+    >
       <Badge count={notifications.length}>
         <BellOutlined
           style={{
             fontSize: '2rem',
+            paddingTop: '0.5rem',
           }}
         />
       </Badge>
