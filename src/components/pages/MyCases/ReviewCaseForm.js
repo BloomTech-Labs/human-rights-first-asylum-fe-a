ed1f58f374f6ca209a712d7e14357d2515005acb
AlexTran0899 <76791231+AlexTran0899@users.noreply.github.com>
2021-08-26 09:55:44 -0500

    added form validation

--- src/components/pages/MyCases/ReviewCaseForm.js
+++ src/components/pages/MyCases/ReviewCaseForm.js
@@ -17,6 +17,7 @@ const ReviewCaseForm = props => {
     setEditedFormValues(formValues);
   }, [formValues, isVisible]);
   const onInputChange = e => {
+    console.log(formValues);
     const { name, value, checked } = e.target;
     if (
       name === 'filed_in_one_year' ||
@@ -32,10 +33,8 @@ const ReviewCaseForm = props => {
   const { handleSubmit } = useForm();
   const onSubmit = evt => {
     delete editedFormValues.id;
-    console.log(editedFormValues);
-    evt.preventDefault();
     axiosWithAuth()
-      .put(`/upload/update/${currentId}`, editedFormValues)
+      .put(`/cases/update/${currentId}`, editedFormValues)
       .then(res => {
         getPendingCases();
         setVisible(false);
@@ -51,131 +50,216 @@ const ReviewCaseForm = props => {
         <Form
           layout="vertical"
           className="user-form"
-          onFinish={handleSubmit(onSubmit)}
+          onFinish={onSubmit}
+          initialValues={{
+            outcome: editedFormValues.outcome,
+            country_of_origin: editedFormValues.country_of_origin,
+            protected_grounds: editedFormValues.protected_grounds,
+            applicant_language: editedFormValues.applicant_language,
+            application_type: editedFormValues.application_type,
+            case_origin_city: editedFormValues.case_origin_city,
+            case_origin_state: editedFormValues.case_origin_state,
+            gender: editedFormValues.gender,
+            indigenous_group: editedFormValues.indigenous_group,
+            type_of_violence: editedFormValues.type_of_violence,
+            initial_or_appellate: editedFormValues.initial_or_appellate,
+            filed_in_one_year: editedFormValues.filed_in_one_year,
+            credible: editedFormValues.credible,
+          }}
         >
           <div>
-            <Form.Item label="Decision Date">
+            <Form.Item label="Decision Date" name="date">
               <DatePicker id="hearing-date" />
             </Form.Item>
           </div>
           <div>
-            <Form.Item label="Case Outcome">
-              <Input
-                id="outcome"
-                type="text"
-                name="outcome"
-                onChange={onInputChange}
-                placeholder="Case Outcome"
-                value={editedFormValues.outcome}
-              />
+            <Form.Item
+              label="Case Outcome"
+              id="outcome"
+              type="text"
+              name="outcome"
+              onChange={onInputChange}
+              placeholder="Case Outcome"
+              value={editedFormValues.outcome}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Case Outcome!',
+                },
+              ]}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div>
-            <Form.Item label="Nation of Origin">
-              <Input
-                id="nation-of-origin"
-                type="text"
-                name="country_of_origin"
-                onChange={onInputChange}
-                placeholder="Nation of Origin"
-                value={editedFormValues.country_of_origin}
-              />
+            <Form.Item
+              label="Nation of Origin"
+              id="nation-of-origin"
+              type="text"
+              name="country_of_origin"
+              onChange={onInputChange}
+              placeholder="Nation of Origin"
+              value={editedFormValues.country_of_origin}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Nation of Origin!',
+                },
+              ]}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div>
-            <Form.Item label="Protected Ground">
-              <Input
-                id="protected-ground"
-                type="text"
-                name="protected_grounds"
-                onChange={onInputChange}
-                placeholder="Protected Ground"
-                value={editedFormValues.protected_grounds}
-              />
+            <Form.Item
+              label="Protected Ground"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Protected Ground!',
+                },
+              ]}
+              id="protected-ground"
+              type="text"
+              name="protected_grounds"
+              onChange={onInputChange}
+              placeholder="Protected Ground"
+              value={editedFormValues.protected_grounds}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div>
-            <Form.Item label="application-type">
-              <Input
-                id="application-type"
-                type="text"
-                name="application_type"
-                onChange={onInputChange}
-                placeholder="Application Type"
-                value={editedFormValues.application_type}
-              />
+            <Form.Item
+              label="application-type"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input application-type!',
+                },
+              ]}
+              id="application-type"
+              type="text"
+              name="application_type"
+              onChange={onInputChange}
+              placeholder="Application Type"
+              value={editedFormValues.application_type}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div>
-            <Form.Item label="Case Origin City">
-              <Input
-                id="case-origin-city"
-                type="text"
-                name="case_origin_city"
-                onChange={onInputChange}
-                placeholder="Case Origin City"
-                value={editedFormValues.case_origin_city}
-              />
+            <Form.Item
+              label="Case Origin City"
+              id="case-origin-city"
+              type="text"
+              name="case_origin_city"
+              onChange={onInputChange}
+              placeholder="Case Origin City"
+              value={editedFormValues.case_origin_city}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Case Origin City',
+                },
+              ]}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div>
-            <Form.Item label="Case Origin State">
-              <Input
-                id="case-origin-state"
-                type="text"
-                name="case_origin_state"
-                onChange={onInputChange}
-                placeholder="Case Origin State"
-                value={editedFormValues.case_origin_state}
-              />
+            <Form.Item
+              label="Case Origin State"
+              id="case-origin-state"
+              type="text"
+              name="case_origin_state"
+              onChange={onInputChange}
+              placeholder="Case Origin State"
+              value={editedFormValues.case_origin_state}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Case Origin State',
+                },
+              ]}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div>
-            <Form.Item label="Applicant Gender">
-              <Input
-                id="applicant-gender"
-                type="text"
-                name="gender"
-                onChange={onInputChange}
-                placeholder="Applicant Gender"
-                value={editedFormValues.gender}
-              />
+            <Form.Item
+              label="Applicant Gender"
+              id="applicant-gender"
+              type="text"
+              name="gender"
+              onChange={onInputChange}
+              placeholder="Applicant Gender"
+              value={editedFormValues.gender}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Applicant Gender',
+                },
+              ]}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div>
-            <Form.Item label="Applicant Language">
-              <Input
-                id="applicant-language"
-                type="text"
-                name="applicant_language"
-                onChange={onInputChange}
-                placeholder="Applicant Language"
-                value={editedFormValues.applicant_language}
-              />
+            <Form.Item
+              label="Applicant Language"
+              id="applicant-language"
+              type="text"
+              name="applicant_language"
+              onChange={onInputChange}
+              placeholder="Applicant Language"
+              value={editedFormValues.applicant_language}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Applicant Language',
+                },
+              ]}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div>
-            <Form.Item label="Applicant Indigenous Group">
-              <Input
-                id="applicant-indigenous-group"
-                type="text"
-                name="indigenous_group"
-                onChange={onInputChange}
-                placeholder="Applicant Indigenous Group"
-                value={editedFormValues.indigenous_group}
-              />
+            <Form.Item
+              label="Applicant Indigenous Group"
+              id="applicant-indigenous-group"
+              type="text"
+              name="indigenous_group"
+              onChange={onInputChange}
+              placeholder="Applicant Indigenous Group"
+              value={editedFormValues.indigenous_group}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Indigenous Group',
+                },
+              ]}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div className="type-of-violence-experienced">
-            <Form.Item label="Type of Violence Experienced">
-              <Input
-                id="type-of-violence-experienced"
-                type="text"
-                name="type_of_violence"
-                onChange={onInputChange}
-                placeholder="Type of Violence Experienced"
-                value={editedFormValues.type_of_violence}
-              />
+            <Form.Item
+              label="Type of Violence Experienced"
+              id="type-of-violence-experienced"
+              type="text"
+              name="type_of_violence"
+              onChange={onInputChange}
+              placeholder="Type of Violence Experienced"
+              value={editedFormValues.type_of_violence}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Type of Violence Experienced',
+                },
+              ]}
+            >
+              <Input />
             </Form.Item>
           </div>
           <div className="checkbox">
@@ -206,7 +290,7 @@ const ReviewCaseForm = props => {
             </Checkbox>
           </div>
           <div className="submit-button">
-            <Button className="button-styles" onClick={onSubmit}>
+            <Button className="button-styles" type="primary" htmlType="submit">
               Submit
             </Button>
           </div>
