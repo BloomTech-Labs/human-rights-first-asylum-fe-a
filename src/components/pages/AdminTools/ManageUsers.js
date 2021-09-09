bf3c2658823326d022b0016e8a32efbe645902e0
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-09-09 09:01:27 -0700

    change manage user divider from p tag to div and change classname to users-divider

--- src/components/pages/AdminTools/ManageUsers.js
+++ src/components/pages/AdminTools/ManageUsers.js
@@ -206,9 +206,9 @@ const ManageUsersPage = props => {
     <div className="users-container">
       <div className="users">
         <h2 className="users-header"> Manage Users </h2>
-        <p className="divider">
+        <div className="users-divider">
           <Icon component={() => <img src={OrangeLine} alt="divider icon" />} />
-        </p>
+        </div>
       </div>
       <Tabs defaultActiveKey="1">
         <TabPane tab="Manage Users" key="1">
